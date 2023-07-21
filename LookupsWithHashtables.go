88989a5dbc375e2edfc5560ead1b7a6b735963b1
Frederick Hieronymus <frederick@disroot.org>
2023-07-22 00:35:00 +0200

    Step 3: Removing Items

--- LookupsWithHashtables.go
+++ LookupsWithHashtables.go
@@ -8,13 +8,13 @@ import (
 func main() {
 	// Make some names.
 	employees := []Employee{
-		{"Ann Archer", "202-555-0101"},
-		{"Bob Baker", "202-555-0102"},
-		{"Cindy Cant", "202-555-0103"},
-		{"Dan Deever", "202-555-0104"},
-		{"Edwina Eager", "202-555-0105"},
-		{"Fred Franklin", "202-555-0106"},
-		{"Gina Gable", "202-555-0107"},
+		{"Ann Archer", "202-555-0101", false},
+		{"Bob Baker", "202-555-0102", false},
+		{"Cindy Cant", "202-555-0103", false},
+		{"Dan Deever", "202-555-0104", false},
+		{"Edwina Eager", "202-555-0105", false},
+		{"Fred Franklin", "202-555-0106", false},
+		{"Gina Gable", "202-555-0107", false},
 	}
 
 	hashTable := NewLinearProbingHashTable(10)
@@ -23,16 +23,28 @@ func main() {
 	}
 	hashTable.dump()
 
+	hashTable.probe("Hank Hardy")
 	fmt.Printf("Table contains Sally Owens: %t\n", hashTable.contains("Sally Owens"))
 	fmt.Printf("Table contains Dan Deever: %t\n", hashTable.contains("Dan Deever"))
-	// fmt.Println("Deleting Dan Deever")
-	// hash_table.delete("Dan Deever")
-	// fmt.Printf("Table contains Dan Deever: %t\n", hash_table.contains("Dan Deever"))
+	fmt.Println("Deleting Dan Deever")
+	hashTable.delete("Dan Deever")
+	fmt.Printf("Table contains Dan Deever: %t\n", hashTable.contains("Dan Deever"))
 	fmt.Printf("Sally Owens: %s\n", hashTable.get("Sally Owens"))
 	fmt.Printf("Fred Franklin: %s\n", hashTable.get("Fred Franklin"))
 	fmt.Println("Changing Fred Franklin")
 	hashTable.set("Fred Franklin", "202-555-0100")
 	fmt.Printf("Fred Franklin: %s\n", hashTable.get("Fred Franklin"))
+	hashTable.dump()
+
+	hashTable.probe("Ann Archer")
+	hashTable.probe("Bob Baker")
+	hashTable.probe("Cindy Cant")
+	hashTable.probe("Dan Deever")
+	hashTable.probe("Edwina Eager")
+	hashTable.probe("Fred Franklin")
+	hashTable.probe("Gina Gable")
+	hashTable.set("Hank Hardy", "202-555-0108")
+	hashTable.probe("Hank Hardy")
 
 	// Look at clustering.
 	random := rand.New(rand.NewSource(12345)) // Initialize with a fixed seed
@@ -63,8 +75,9 @@ func hash(value string) int {
 }
 
 type Employee struct {
-	name  string
-	phone string
+	name    string
+	phone   string
+	deleted bool
 }
 
 type LinearProbingHashTable struct {
@@ -84,7 +97,11 @@ func NewLinearProbingHashTable(capacity int) *LinearProbingHashTable {
 func (hashTable *LinearProbingHashTable) dump() {
 	for key, employee := range hashTable.employees {
 		if employee != nil {
-			fmt.Printf("%2d: %-15s %s\n", key, employee.name, employee.phone)
+			if employee.deleted {
+				fmt.Printf("%2d: xxx\n", key)
+			} else {
+				fmt.Printf("%2d: %-15s %s\n", key, employee.name, employee.phone)
+			}
 		} else {
 			fmt.Printf("%2d: ---\n", key)
 		}
@@ -96,12 +113,20 @@ func (hashTable *LinearProbingHashTable) dump() {
 // If the key is not present and the table is full, return -1 for the index.
 func (hashTable *LinearProbingHashTable) find(name string) (int, int) {
 	computedHash := hash(name) % hashTable.capacity
+	deletedIndex := -1
 
 	for i := 0; i < hashTable.capacity; i++ {
 		index := (computedHash + i) % hashTable.capacity
 
 		if hashTable.employees[index] == nil {
+			if deletedIndex >= 0 {
+				return deletedIndex, i + 1
+			}
 			return index, i + 1
+		}
+
+		if deletedIndex == -1 && hashTable.employees[index].deleted {
+			deletedIndex = index
 		}
 
 		if hashTable.employees[index].name == name {
@@ -109,6 +134,9 @@ func (hashTable *LinearProbingHashTable) find(name string) (int, int) {
 		}
 	}
 
+	if deletedIndex >= 0 {
+		return deletedIndex, hashTable.capacity
+	}
 	return -1, hashTable.capacity
 }
 
@@ -120,10 +148,10 @@ func (hashTable *LinearProbingHashTable) set(name string, phone string) {
 		panic("The hashtable is full.")
 	}
 
-	if hashTable.employees[index] != nil {
-		hashTable.employees[index].phone = phone
+	if hashTable.employees[index] == nil || hashTable.employees[index].deleted {
+		hashTable.employees[index] = &Employee{name, phone, false}
 	} else {
-		hashTable.employees[index] = &Employee{name, phone}
+		hashTable.employees[index].phone = phone
 	}
 }
 
@@ -138,6 +166,11 @@ func (hashTable *LinearProbingHashTable) get(name string) string {
 	if hashTable.employees[index] == nil {
 		return ""
 	}
+
+	if hashTable.employees[index].deleted {
+		return ""
+	}
+
 	return hashTable.employees[index].phone
 }
 
@@ -152,7 +185,21 @@ func (hashTable *LinearProbingHashTable) contains(name string) bool {
 	if hashTable.employees[index] == nil {
 		return false
 	}
+
+	if hashTable.employees[index].deleted {
+		return false
+	}
+
 	return true
+}
+
+// Delete this key's entry.
+func (hashTable *LinearProbingHashTable) delete(name string) {
+	index, _ := hashTable.find(name)
+
+	if index >= 0 && hashTable.employees[index] != nil {
+		hashTable.employees[index].deleted = true
+	}
 }
 
 // Make a display showing whether each slice entry is nil.
@@ -164,7 +211,11 @@ func (hashTable *LinearProbingHashTable) dumpConcise() {
 			fmt.Printf(".")
 		} else {
 			// Display this entry.
-			fmt.Printf("O")
+			if !employee.deleted {
+				fmt.Printf("O")
+			} else {
+				fmt.Printf("x")
+			}
 		}
 		if i%50 == 49 {
 			fmt.Println()
@@ -185,4 +236,67 @@ func (hashTable *LinearProbingHashTable) aveProbeSequenceLength() float32 {
 		}
 	}
 	return float32(totalLength) / float32(numValues)
+}
+
+// Show this key's probe sequence.
+func (hashTable *LinearProbingHashTable) probe(name string) int {
+	// Hash the key.
+	hash := hash(name) % hashTable.capacity
+	fmt.Printf("Probing %s (%d)\n", name, hash)
+
+	// Keep track of a deleted spot if we find one.
+	deletedIndex := -1
+
+	// Probe up to hash_table.capacity times.
+	for i := 0; i < hashTable.capacity; i++ {
+		index := (hash + i) % hashTable.capacity
+
+		fmt.Printf("    %d: ", index)
+		if hashTable.employees[index] == nil {
+			fmt.Printf("---\n")
+		} else if hashTable.employees[index].deleted {
+			fmt.Printf("xxx\n")
+		} else {
+			fmt.Printf("%s\n", hashTable.employees[index].name)
+		}
+
+		// If this spot is empty, the value isn't in the table.
+		if hashTable.employees[index] == nil {
+			// If we found a deleted spot, return its index.
+			if deletedIndex >= 0 {
+				fmt.Printf("    Returning deleted index %d\n", deletedIndex)
+				return deletedIndex
+			}
+
+			// Return this index, which holds nil.
+			fmt.Printf("    Returning nil index %d\n", index)
+			return index
+		}
+
+		// If this spot is deleted, remember where it is.
+		if hashTable.employees[index].deleted {
+			if deletedIndex < 0 {
+				deletedIndex = index
+			}
+		} else if hashTable.employees[index].name == name {
+			// If this cell holds the key, return its data.
+			fmt.Printf("    Returning found index %d\n", index)
+			return index
+		}
+
+		// Otherwise continue the loop.
+	}
+
+	// If we get here, then the key is not
+	// in the table and the table is full.
+
+	// If we found a deleted spot, return it.
+	if deletedIndex >= 0 {
+		fmt.Printf("    Returning deleted index %d\n", deletedIndex)
+		return deletedIndex
+	}
+
+	// There's nowhere to put a new entry.
+	fmt.Printf("    Table is full\n")
+	return -1
 }
